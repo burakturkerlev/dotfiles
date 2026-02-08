5ef2a5099d472d8dd355a9917b0bda0d40644aac
burakturkerlev <burakturkerlev@gmail.com>
2026-02-08 22:03:32 +0300

    improved

--- nvim/lua/burakturkerdev/plugins/lsp.lua
+++ nvim/lua/burakturkerdev/plugins/lsp.lua
@@ -1,31 +1,22 @@
 return {
   "neovim/nvim-lspconfig",
-  dependencies = {
-    "williamboman/mason.nvim",
-    "williamboman/mason-lspconfig.nvim",
-  },
   config = function()
-    require("mason").setup()
-    require("mason-lspconfig").setup({
-      ensure_installed = {
-        "clangd",     -- C/C++
-        "rust_analyzer", -- Rust
-        "gopls",      -- Golang
-      }
+    -- Enable built-in LSP configs (Nvim 0.11+)
+    vim.lsp.enable({
+      "clangd",
+      "rust_analyzer",
+      "gopls",
     })
 
-    vim.lsp.enable('clangd')
-    vim.lsp.enable('rust_analyzer')
-    vim.lsp.enable('gopls')
-
-    vim.api.nvim_create_autocmd('LspAttach', {
+    -- Keymaps when LSP attaches
+    vim.api.nvim_create_autocmd("LspAttach", {
       callback = function(args)
         local opts = { buffer = args.buf }
-        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-        vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-        vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
-        vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
-        vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+        vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+        vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+        vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+        vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
       end,
     })
   end,
--- nvim/lua/burakturkerdev/remap.lua
+++ nvim/lua/burakturkerdev/remap.lua
@@ -3,3 +3,9 @@ vim.g.mapleader = ";"
 
 -- Save with leader + w
 vim.keymap.set('n', '<Leader>w', ':w<CR>')
+
+-- Exit with leader + pv
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- Exit insert mode with jk
+vim.keymap.set("i", "jk", "<Esc>", { noremap = true, silent = true })
